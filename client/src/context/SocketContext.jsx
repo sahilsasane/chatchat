4fe19c532c4cecbc4c309720032a16cb815b2c61
sahil <shsasane22@gmail.com>
2024-06-18 00:15:44 +0530

    socket

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketProvider = ({ children }) => {
     const { user } = useAuthContext()
     useEffect(() => {
         if (user) {
-            const socket = io("http://localhost:5000", {
+            const socket = io("https://chatchat-m052.onrender.com", {
                 query: {
                     userId: user._id
                 }
